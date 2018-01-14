3b221eaa4ede3154ae78721291873aa953a2cd7c
FelixDracul <33679434+FelixDracul@users.noreply.github.com>
2018-01-15 00:17:52 +0530

    House Cleaning
    Unwanted comments, images, and files removed. Small fixes to some files. Doing so may have added more bugs that might come back unexpectedly and bite us in the ass.

--- config.php
+++ config.php
@@ -1,26 +1,13 @@
 <?php
-/******************************************************
-------------------Required Configuration---------------
-Please edit the following variables so the forum can
-work correctly.
-******************************************************/
 
-/*We log to the DataBase
-$conn=mysqli_connect('localhost', 'root', '');
-mysqli_select_db('$conn','forum_db'); */
-
-//1. Create a database connection
+// Create a database connection
 $connection = mysqli_connect('localhost','root','');
 
-// 2. Select a database to use 
+// Select a database to use 
 $db_select = mysqli_select_db($connection, 'ssif_forum_db');
 
 //Username of the Administrator
 $admin='mafia17.1';
-
-/******************************************************
------------------Optional Configuration----------------
-******************************************************/
 
 //Forum Home Page
 $url_home = 'index.php';
@@ -29,8 +16,5 @@ $url_home = 'index.php';
 $design = 'default';
 
 
-/******************************************************
-----------------------Initialization-------------------
-******************************************************/
 include('init.php');
 ?>
